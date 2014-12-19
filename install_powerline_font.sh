2cbe14d561b86e4213eafcfcb366d47f4e0e7437
Herb Jiang <herb.jiang@gmail.com>
2014-12-20 03:48:41 +0800

    fix script

--- install_powerline_font.sh
+++ install_powerline_font.sh
@@ -1,4 +1,4 @@
 mkdir /tmp/git-clone
-git clone https://github.com/Lokaltog/powerline-fonts /tmp/git-clone
-sudo mv /tmp/git-clone/powerline-fonts /usr/share/fonts/powerline-fonts
+git clone https://github.com/Lokaltog/powerline-fonts /tmp/powerline-fonts
+sudo mv /tmp/powerline-fonts /usr/share/fonts/powerline-fonts
 sudo fc-cache -v -f
